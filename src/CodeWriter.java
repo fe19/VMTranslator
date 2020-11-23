d63a1ec3ee5359e35c438db38ff07d7d3d0e6a02
Fabian Engler <fabian.engler@ergon.ch>
2020-11-23 18:54:04 +0100

    Implement all arithmetic and logic operations

--- src/CodeWriter.java
+++ src/CodeWriter.java
@@ -8,6 +8,13 @@ public class CodeWriter {
 
     FileWriter fileWriter;
 
+    int countEQ = 0;
+    int countGT = 0;
+    int countLT = 0;
+    int countAnd = 0;
+    int countOr = 0;
+    int countNot = 0;
+
     public CodeWriter(String filename) throws IOException {
         fileWriter = new FileWriter(filename);
     }
@@ -18,13 +25,13 @@ public class CodeWriter {
         // Assembly for push command
         if (command.equals("push")) {
             if (segment.equals("constant")) {
-                fileWriter.write("@" + index+ "\n");
-                fileWriter.write("D=A\n");
-                fileWriter.write("@SP\n");
-                fileWriter.write("A=M\n");
-                fileWriter.write("M=D\n");
-                fileWriter.write("@SP\n");
-                fileWriter.write("M=M+1\n");
+                fileWriter.write("   @" + index + "\n");
+                fileWriter.write("   D=A\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=D\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
             }
         }
     }
@@ -33,22 +40,203 @@ public class CodeWriter {
         // Add comment first to indicate VM commad that is followed by HACK assembly
         fileWriter.write("// " + command + "\n");
         // Assembly
-        switch (command){
+        switch (command) {
             case "add":
-                fileWriter.write("@SP\n");
-                fileWriter.write("M=M-1\n");
-                fileWriter.write("A=M\n");
-                fileWriter.write("D=M\n");
-                fileWriter.write("@SP\n");
-                fileWriter.write("M=M-1\n");
-                fileWriter.write("A=M\n");
-                fileWriter.write("D=D+M\n");
-                fileWriter.write("M=D\n");
-                fileWriter.write("@SP\n");
-                fileWriter.write("M=M+1\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=D+M\n");
+                fileWriter.write("   M=D\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
                 break;
             case "sub":
-                fileWriter.write("@SP\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=D-M\n");
+                fileWriter.write("   M=D\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
+                break;
+            case "neg":
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   D=-D\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=D\n");
+                break;
+            case "eq":
+                String labelEQ = "EQ" + countEQ;
+                String labelEQEnd = "EQEND" + countEQ;
+                countEQ++;
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M-D\n");
+                fileWriter.write("   @" + labelEQ + "\n");
+                fileWriter.write("   D;JEQ\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=0\n");
+                fileWriter.write("   @" + labelEQEnd + "\n");
+                fileWriter.write("   0;JMP\n");
+                fileWriter.write("(" + labelEQ + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=1\n");
+                fileWriter.write("(" + labelEQEnd + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
+                break;
+            case "gt":
+                String labelGT = "GT" + countGT;
+                String labelGTEnd = "GTEND" + countGT;
+                countGT++;
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M-D\n");
+                fileWriter.write("   @" + labelGT + "\n");
+                fileWriter.write("   D;JGT\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=0\n");
+                fileWriter.write("   @" + labelGTEnd + "\n");
+                fileWriter.write("   0;JMP\n");
+                fileWriter.write("(" + labelGT + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=1\n");
+                fileWriter.write("(" + labelGTEnd + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
+                break;
+            case "lt":
+                String labelLT = "GT" + countLT;
+                String labelLTEnd = "GTEND" + countLT;
+                countLT++;
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M-D\n");
+                fileWriter.write("   @" + labelLT + "\n");
+                fileWriter.write("   D;JLT\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=0\n");
+                fileWriter.write("   @" + labelLTEnd + "\n");
+                fileWriter.write("   0;JMP\n");
+                fileWriter.write("(" + labelLT + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=1\n");
+                fileWriter.write("(" + labelLTEnd + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
+                break;
+            case "and":
+                String labelAnd = "AND" + countAnd;
+                String labelAndEnd = "ANDEND" + countAnd;
+                countAnd++;
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @" + labelAnd + "\n");
+                fileWriter.write("   D;JEQ\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @" + labelAnd + "\n");
+                fileWriter.write("   D;JEQ\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=1\n");
+                fileWriter.write("   @" + labelAndEnd + "\n");
+                fileWriter.write("   0;JMP\n");
+                fileWriter.write("(" + labelAnd + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=0\n");
+                fileWriter.write("(" + labelAndEnd + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
+                break;
+            case "or":
+                String labelOr = "AND" + countOr;
+                String labelOrEnd = "OREND" + countOr;
+                countOr++;
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @" + labelOr + "\n");
+                fileWriter.write("   D;JGT\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @" + labelOr + "\n");
+                fileWriter.write("   D;JGT\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=0\n");
+                fileWriter.write("   @" + labelOrEnd + "\n");
+                fileWriter.write("   0;JMP\n");
+                fileWriter.write("(" + labelOr + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=1\n");
+                fileWriter.write("(" + labelOrEnd + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
+                break;
+            case "not":
+                String labelNot = "NOT" + countNot;
+                String labelNotEnd = "NOTEND" + countNot;
+                countNot++;
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M-1\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   D=M\n");
+                fileWriter.write("   @" + labelNot + "\n");
+                fileWriter.write("   D;JEQ\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=0\n");
+                fileWriter.write("   @" + labelNotEnd + "\n");
+                fileWriter.write("   0;JMP\n");
+                fileWriter.write("(" + labelNot + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   A=M\n");
+                fileWriter.write("   M=1\n");
+                fileWriter.write("(" + labelNotEnd + ")\n");
+                fileWriter.write("   @SP\n");
+                fileWriter.write("   M=M+1\n");
                 break;
             default:
                 throw new IllegalStateException("Unexpected value: " + command);
