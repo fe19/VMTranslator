7b19c06a610c271d605e399683772a3837ad77b5
Fabian Engler <fabian.engler@ergon.ch>
2020-11-24 18:44:55 +0100

    Complete all five tests successfully

--- SimpleAdd.asm
+++ SimpleAdd.asm
@@ -1,28 +1,35 @@
 // push constant 7
-@7
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M+1
+   // *SP = i
+   @7
+   D=A
+   @SP
+   A=M
+   M=D
+   // SP++
+   @SP
+   M=M+1
 // push constant 8
-@8
-D=A
-@SP
-A=M
-M=D
-@SP
-M=M+1
+   // *SP = i
+   @8
+   D=A
+   @SP
+   A=M
+   M=D
+   // SP++
+   @SP
+   M=M+1
 // add
-@SP
-M=M-1
-A=M
-D=M
-@SP
-M=M-1
-A=M
-D=D+M
-M=D
-@SP
-M=M+1
+   // SP--
+   @SP
+   M=M-1
+   // SP = X + Y
+   A=M
+   D=M
+   @SP
+   M=M-1
+   A=M
+   D=D+M
+   M=D
+   // SP++
+   @SP
+   M=M+1
