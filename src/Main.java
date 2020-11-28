0b4f051d822173285924db662b332d5f6c0009d2
Fabian Engler <fabian.engler@ergon.ch>
2020-11-28 14:32:48 +0100

    Import FibonacciElement test case

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,19 @@
+import java.io.File;
 import java.io.IOException;
 
 public class Main {
 
     public static void main(String[] args) throws IOException {
 
-        Parser parser = new Parser("test/SimpleFunction");
-        parser.advance();
+        String fileName = "test/FibonacciElement";
+        File file = new File(fileName);
+
+        if (file.isDirectory()) {
+            System.out.println("Directory");
+        } else {
+            Parser parser = new Parser(fileName);
+            parser.advance();
+        }
+
     }
 }
